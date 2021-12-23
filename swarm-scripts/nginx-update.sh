ad1fb428cbe064d7445a643c74615bb81911b27e
Sebastian Plocek <sebastian@plocek.at>
2021-12-23 14:50:08 +0100

    Fix issue "the input device is not a TTY".

--- swarm-scripts/nginx-update.sh
+++ swarm-scripts/nginx-update.sh
@@ -5,5 +5,5 @@
 #
 
 SERVICE="proxystack_nginx"
-docker exec -it $(docker ps --filter "name=$SERVICE" -q --no-trunc | head -n1) nginx -t
-docker service update --force --update-parallelism 1 --update-delay 30s $SERVICE
+docker exec $(docker ps --filter "name=$SERVICE" -q --no-trunc | head -n1) nginx -t
+docker service update --force --quiet --update-parallelism 1 --update-delay 30s $SERVICE
